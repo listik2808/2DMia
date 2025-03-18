9fe558dc5d2ef4eb6f3e15b4c089b27812e26a5c
Евгений <listopadov_94@mail.ru>
2025-03-18 17:04:35 +0300

    движение камеры и перемещение яблока

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -3,4 +3,26 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
+    [SerializeField] private float _speed;
+    private Vector2 _startPosition;
+    private float _targetPosition;
+
+    private void Start()
+    {
+        _targetPosition = transform.position.x;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _startPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float pos = _camera.ScreenToWorldPoint(Input.mousePosition).x - _startPosition.x;
+            _targetPosition = Mathf.Clamp(transform.position.x - pos, -557f, 838f);
+        }
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, _targetPosition, _speed * Time.deltaTime),transform.position.y,transform.position.z);
+    }
 }
---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas _canvas;
+    private RectTransform _rectTransform;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+}
